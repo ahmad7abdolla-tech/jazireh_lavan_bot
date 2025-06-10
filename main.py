915c75bd16c426135908ec56247440c5d8cea654
ahmad7abdolla-tech <ahmad.9h9+1@gmail.com>
2025-06-10 20:11:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,37 @@ from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 from bot.weather_today import handle_weather_today
 
-
 BOT_TOKEN = "7586578372:AAEIkVr4Wq23NSkLuSPRl1yqboqd7_cW0ac"
 
-keyboard = [["هوای لاوان الان چطوره؟"]]
+keyboard = [
+    ["هوای لاوان الان چطوره؟🌦"],
+    ["📍لوکیشن‌های جزیره لاوان"]  # دکمه جدید
+]
 reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-# دستور شروع /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "سلام! 👋 \n حیاکم الله😉 به ربات جزیره لاوان خوش آمدی.\n\nیکی از دکمه‌ها رو انتخاب کن:",
         reply_markup=reply_markup
     )
 
-# هندلر برای پیام‌های متنی
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
-    if text == "هوای لاوان الان چطوره؟🌦️":
+    if text == "هوای لاوان الان چطوره؟🌦":
         await update.message.reply_text("⏳ در حال دریافت اطلاعات هواشناسی...")
         response = handle_weather_today()
         await update.message.reply_text(response)
+    
+    elif text == "📍لوکیشن‌های جزیره لاوان":  # پاسخ موقت
+        await update.message.reply_text("در حال توسعه است ⏳")
+        
     else:
         await update.message.reply_text("لطفاً از دکمه‌های موجود استفاده کن.")
 
-# اجرای ربات
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
     print("🤖 ربات با موفقیت اجرا شد.")
     app.run_polling()
