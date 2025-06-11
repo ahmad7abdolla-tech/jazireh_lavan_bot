07338877716a1fab3744fcad7da567a8992c3813
ahmad7abdolla-tech <ahmad.9h9+1@gmail.com>
2025-06-11 13:37:51 +0330

    Update locations.py

--- bot/locations.py
+++ bot/locations.py
@@ -1,1 +1,16 @@
+# bot/locations.py
+from bot.admins import is_admin
+from bot.google_drive import upload_image
 
+locations = []
+
+def add_location(admin_id: int, name: str, photo_path: str):
+    if not is_admin(admin_id):
+        return False
+    
+    photo_url = upload_image(photo_path)
+    locations.append({"name": name, "photo": photo_url})
+    return True
+
+def get_locations():
+    return locations
