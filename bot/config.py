29ba4c0a655556e7b8def88aed0c13b3ae4bf1fd
ahmad7abdolla-tech <ahmad.9h9+1@gmail.com>
2025-06-12 01:26:26 +0330

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,13 @@ import os
 
 # تنظیمات دیتابیس
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-DATABASE_NAME = "lavan_island"
+DATABASE_NAME = os.getenv("DATABASE_NAME", "lavan_island")
 
 # محدودیت‌ها
 MAX_IMAGE_SIZE_MB = 5  # حداکثر حجم تصاویر بر حسب مگابایت
+
+# توکن ربات تلگرام
+TELEGRAM_BOT_TOKEN = "7586578372:AAEIkVr4Wq23NSkLuSPRl1yqboqd7_cW0ac"
+
+# سطح لاگینگ
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
